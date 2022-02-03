927df2e247ca8ff489df92dd599c5b8f615fd6e0
Daskendrik <ss599@yandex.ru>
2022-02-03 22:46:33 +0300

    basis of the web server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express'); //установили зависимость экспресса (подклюили)
+const app = express(); //создали объект
+const port = process.env.port || 4000; // переменная отвечает за порт, если его нет, то локально
+
+app.get('/', (req,res) => res.send('Privetiki! =)')); //используем метод гет объекта эп, чтобы отправить ответ
+app.listen(port, () => console.log(`Прослушиваем порт http://localhost:${port}!`)) // приложение слушает порт из константы
