2d51a78769aa41d06af9c6dd8b97875a9a5e478e
Daskendrik <ss599@yandex.ru>
2022-02-05 17:07:27 +0300

    create GraphQL API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const express = require('express'); //установили зависимость экспресса (подклюили)
+const {ApolloServer, gql} = require('apollo-server-express'); //включили пакет 
 const app = express(); //создали объект
 const port = process.env.port || 4000; // переменная отвечает за порт, если его нет, то локально
 
-app.get('/', (req,res) => res.send('Privetiki! =)')); //используем метод гет объекта эп, чтобы отправить ответ
-app.listen(port, () => console.log(`Прослушиваем порт http://localhost:${port}!`)) // приложение слушает порт из константы
+// app.get('/', (req,res) => res.send('Privetiki! =)')); //используем метод гет объекта эп, чтобы отправить ответ
+// app.listen(port, () => console.log(`Прослушиваем порт http://localhost:${port}!`)) // приложение слушает порт из константы
+
+//Построение схемы с использованием языка схем GraphQL
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+//Добавили распознователь
+const resolvers = {
+ Query: {
+    hello: () => 'Hello world!'
+ }
+ };
+
+//Настройка Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+//Применяем промежуточное ПО Apollo GraphQL и указываем путь к /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({port}, () =>
+    console.log(`Мы запсутили GraphQL Server на нашем порте`)
+);
